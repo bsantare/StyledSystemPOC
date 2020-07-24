72b5a9b6f6ab9b6ca154e7322e1fdd24e8e1593b
James Dunnam <7660553+jimador@users.noreply.github.com>
2020-07-24 17:42:57 -0400

    use check icon

--- src/component/checkbox/checkbox-icon.tsx
+++ src/component/checkbox/checkbox-icon.tsx
@@ -2,6 +2,7 @@
 import { jsx, css } from '@emotion/core';
 
 import styled from '@emotion/styled';
+import { ReactComponent as Check } from '../../icons/check.svg';
 import { SVG } from '../svg';
 import { color } from '../../theme/shared';
 
@@ -27,11 +28,15 @@ const Icon = styled(SVG)`
   stroke: white;
   stroke-width: 2px;
 `;
+const IconCheck = styled(Check)`
+  fill: none;
+  stroke: white;
+  stroke-width: 2px;
+  url
+`;
 
 export const CheckBoxChecked = (props: any) => (
-  <Icon {...props} css={checkedCss}>
-    <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-  </Icon>
+  <IconCheck {...props} css={checkedCss} />
 );
 
 export const CheckboxUnchecked = (props: any) => (
